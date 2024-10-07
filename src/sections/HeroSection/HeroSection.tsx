6c1be379fdc26fdc67aa915079a77097b47e6067
n.bakumenko <n.bakumenko@numedy.com>
2024-10-07 12:17:11 +0300

    feat: add tabs switching in hero section

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -1,8 +1,26 @@
+'use client'
+
 import {SectionWrapper} from "@/components/SectionWrapper/ui/SectionWrapper"
 import s from "@/sections/HeroSection/HeroSection.module.scss"
-import {Header} from "@/components/Header/ui/Header";
+import {Header} from "@/components/Header/ui/Header"
+import {useState} from "react";
+
+const tabs = {
+  1: {
+    id: 1,
+    title: 'Операторам',
+    text: 'Найдите идеальный баланс выручки и удовлетворённости пользователей с платформой рекламной монетизации'
+  },
+  2: {
+    id: 2,
+    title: 'ОТТ сервисам',
+    text: 'Дополнительные возможности обогащения данных об аудитории и монетизации'
+  }
+}
 
 export const HeroSection = () => {
+  const [activeTab, setActiveTab] = useState(1)
+
   return (
     <SectionWrapper backgroundColor={'dark'} backgroundImage={'/hero-bg.png'} sectionClass={s.hero}>
       <Header />
@@ -27,15 +45,18 @@ export const HeroSection = () => {
         </p>
         <div className={s.tabsGroup}>
           <div className={s.tabs}>
-            <button className={s.tab}>
-              Операторам
-            </button>
-            <button className={s.tab}>
-              ОТТ сервисам
-            </button>
+            {Object.values(tabs).map((tab) => (
+              <button
+                key={tab.id}
+                className={activeTab === tab.id ? (s.tab + ' ' + s.tabActive) : s.tab}
+                onClick={() => setActiveTab(tab.id)}
+              >
+                {tab.title}
+              </button>
+            ))}
           </div>
           <div className={s.tabContent}>
-            Найдите идеальный баланс выручки и удовлетворённости пользователей с платформой рекламной монетизации
+            {tabs[activeTab as keyof typeof tabs].text}
           </div>
         </div>
       </div>
